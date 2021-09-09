dce2fa39a78f8ebe01ceb6eac04783ea6361483c
bodatman <82938768+bodatman@users.noreply.github.com>
2021-09-09 08:15:22 +0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,16 @@
+mport discord, os, keep_alive, asyncio, datetime, pytz
+
+from discord.ext import tasks, commands
+
+client = commands.Bot(command_prefix=':', self_bot=True)
+
+
+@client.event
+async def on_connect():
+    await client.change_presence(
+        activity=discord.Streaming(name="AFK ┊ Belajar.",
+                                   url="https://www.twitch.tv/astroboyszz"))
+
+
+keep_alive.keep_alive()
+client.run(os.getenv("TOKEN"), bot=False)
